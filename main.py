6dc1b38367473c9faef802906dadd056dfd9c82e
Bharathpeddi18 <peddibharath07@gmail.com>
2026-09-12 17:08:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ frontend_url = os.getenv("FRONTEND_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=frontend_url,
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
